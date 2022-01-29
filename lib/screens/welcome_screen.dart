8449e55981ed44edde575aabf5cae7a57b53b847
shamuelxd4- <50058913+shamuelxd4@users.noreply.github.com>
2022-01-29 16:51:03 -0400

    ultimo commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const id = 'welcomescreen';
@@ -9,69 +12,108 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  child: Image.asset('images/openinfo.jpg'),
+                  height: 130.0,
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
+              ),
+              // Text(
+              //   'OpenInfo Chat',
+              //   style: TextStyle(
+              //     fontSize: 35.0,
+              //     fontWeight: FontWeight.w900,
+              //     color: Colors.black,
+              //   ),
+              //   textAlign: TextAlign.center,
+              // ),
+              // SizedBox(
+              //   height: 48.0,
+              // ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Material(
+                  elevation: 10.0,
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: MaterialButton(
+                    onPressed: () {
+                      //Go to login screen.
+                      Navigator.pushNamed(context, LoginScreen.id);
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: Text(
+                      'Iniciar Sesion',
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Material(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(30.0),
+                  elevation: 10.0,
+                  child: MaterialButton(
+                    onPressed: () {
+                      //Go to registration screen.
+                      Navigator.pushNamed(context, RegistrationScreen.id);
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: Text(
+                      'Registrar Usuario',
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
+                child: Text(
+                  'Or via soocial media',
+                  // "O Redes Sociales",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              SignInButton(
+                Buttons.Google,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                elevation: 10,
+                onPressed: () {},
+              ),
+              // SignInButton(
+              //   Buttons.Apple,
+              //   shape: RoundedRectangleBorder(
+              //     borderRadius: BorderRadius.circular(20),
+              //   ),
+              //   elevation: 10,
+              //   onPressed: () {},
+              // ),
+              SignInButton(
+                Buttons.Facebook,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                elevation: 10,
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
